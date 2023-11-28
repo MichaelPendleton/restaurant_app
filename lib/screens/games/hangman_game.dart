84f8c3206a685762fbd4ad3e6ef0a466d0a70803
kuhlinger <karamckenzie.uhlinger@calbaptist.edu>
2023-11-28 13:50:48 -0800

    hangman game

--- lib/screens/games/hangman_game.dart
+++ lib/screens/games/hangman_game.dart
@@ -1,42 +1,233 @@
-// ignore_for_file: library_private_types_in_public_api
-
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/models/words.dart';
+
+class AppColor {
+  static Color primaryColor = const Color(0XFF39298a);
+  static Color primaryColorDark = const Color(0xFF231954);
+}
+
+class Game {
+  static int tries = 0;
+  static List<String> selectedChar = [];
+}
+
+Widget figureImage(bool visible, String path) {
+  return Visibility(
+    visible: visible,
+    child: Container(
+      width: 250,
+      height: 250,
+      child: Image.asset(path),
+    ),
+  );
+}
+
+Widget letter(String character, bool hidden) {
+  return Container(
+    height: 65,
+    width: 50,
+    padding: const EdgeInsets.all(12.0),
+    decoration: BoxDecoration(
+      color: AppColor.primaryColorDark,
+      borderRadius: BorderRadius.circular(4.0),
+    ),
+    child: Visibility(
+      visible: !hidden,
+      child: Text(
+        character,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 40.0,
+        ),
+      ),
+    ),
+  );
+}
 
 class HangmanGame extends StatefulWidget {
-  const HangmanGame({super.key});
+  const HangmanGame({Key? key}) : super(key: key);
 
   @override
   _HangmanGameState createState() => _HangmanGameState();
 }
 
 class _HangmanGameState extends State<HangmanGame> {
+  String word = _getRandomWord().toUpperCase();
+  List<String> alphabets = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+
+  static String _getRandomWord() {
+    List<String> words = WordList.words;
+    Random random = Random();
+    return words[random.nextInt(words.length)];
+  }
+
+  bool isWordGuessed() {
+    return word.split('').every((letter) {
+      return Game.selectedChar.contains(letter.toUpperCase());
+    });
+  }
+
+  bool isGameOver() {
+    return Game.tries >= 6; // Adjust the threshold as needed
+  }
+
+  void resetGame() {
+    setState(() {
+      word = _getRandomWord().toUpperCase();
+      Game.tries = 0;
+      Game.selectedChar.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Padding(
-            padding: EdgeInsets.only(left: 5), // Adjust padding as needed
-            child: Text("Hangman"),
-          ),
-          leading: Padding(
-            padding:
-                const EdgeInsets.only(right: 5), // Adjust padding as needed
-            child: Row(
+    return Scaffold(
+      backgroundColor: AppColor.primaryColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            if (Navigator.canPop(context)) {
+              Navigator.maybePop(context);
+            } else {
+              print("Nowhere to pop");
+            }
+          },
+        ),
+        title: const Text("Hangman"),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: AppColor.primaryColor,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Stack(
               children: [
-                IconButton(
-                  icon: const Icon(Icons.arrow_back), // Drawer icon
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
+                figureImage(Game.tries >= 0, "assets/hang.png"),
+                figureImage(Game.tries >= 1, "assets/head.png"),
+                figureImage(Game.tries >= 2, "assets/body.png"),
+                figureImage(Game.tries >= 3, "assets/ra.png"),
+                figureImage(Game.tries >= 4, "assets/la.png"),
+                figureImage(Game.tries >= 5, "assets/rl.png"),
+                figureImage(Game.tries >= 6, "assets/ll.png"),
               ],
             ),
           ),
-        ),
-        body: const Center(
-          child: Text("Hangman"),
-        ),
+          isWordGuessed()
+              ? const Text(
+                  'You won!',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              : Container(),
+          isGameOver()
+              ? const Text(
+                  'Try again!',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              : Container(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: word
+                .split('')
+                .map((e) => letter(e.toUpperCase(),
+                    !Game.selectedChar.contains(e.toUpperCase())))
+                .toList(),
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 250.0,
+            child: GridView.count(
+              crossAxisCount: 7,
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+              padding: const EdgeInsets.all(8.0),
+              children: alphabets.map((e) {
+                return RawMaterialButton(
+                  onPressed: Game.selectedChar.contains(e)
+                      ? null
+                      : () {
+                          setState(() {
+                            Game.selectedChar.add(e);
+                            if (!word.split('').contains(e.toUpperCase())) {
+                              Game.tries++;
+                            }
+                          });
+                        },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  fillColor: Game.selectedChar.contains(e)
+                      ? Colors.black87
+                      : Colors.blue,
+                  child: Text(
+                    e,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          // Reset button
+          RawMaterialButton(
+            onPressed: resetGame,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+            fillColor: Colors.red, // You can change the color as needed
+            child: const Text(
+              'Reset',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
